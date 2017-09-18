397a6abf20f981f546e56b1158ac0586ffb47328
rdrgn <reminjp@gmail.com>
2017-09-18 14:17:18 +0900

    fix path to exec.py

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-var child_process = require('child_process');
+const path = require('path');
+const child_process = require('child_process');
 
-var exec = function (method, args) {
+const path_to_execpy = __dirname + path.sep + 'exec.py';
+
+const exec = function(method, args) {
 	if (!Array.isArray(args)) {
 		args = [];
 	}
-	var input = JSON.stringify({ method: method, args: args });
-	var result = child_process.execFileSync('python', ['exec.py'], { input: input });
+	let input = JSON.stringify({ method: method, args: args });
+	let result = child_process.execFileSync('python', [path_to_execpy], {
+		input: input,
+	});
 	return JSON.parse(result);
-}
+};
 
 exports.exec = exec;
 
-exports.analyzeFile = function (path) {
+exports.analyzeFile = function(path) {
 	return exec('analyze_file', [path]);
-}
+};
 
-exports.analyzeSourceCode = function (name, code) {
+exports.analyzeSourceCode = function(name, code) {
 	return exec('analyze_file.analyze_source_code', [name, code]);
-}
-
+};
