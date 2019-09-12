488adb8bad361eebb5cd9c375135cc34611fab73
remin <noreply@remin.jp>
2019-09-13 04:32:45 +0900

    add test implementation

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,31 +1,22 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import * as lizard from '../src';
 
-const expectedResult = {
-  filename: 'fizzbuzz.cpp',
-  nloc: 15,
-  function_list: [
-    {
-      cyclomatic_complexity: 6,
-      nloc: 13,
-      token_count: 89,
-      name: 'main',
-      long_name: 'main()',
-      start_line: 3,
-      end_line: 15,
-      parameters: [],
-      filename: 'fizzbuzz.cpp',
-      top_nesting_level: 0,
-      length: 13,
-      fan_in: 0,
-      fan_out: 0,
-      general_fan_out: 0,
-    },
-  ],
-  token_count: 96,
-};
+describe('test', () => {
+  const inputPath = path.resolve(__dirname, 'input.cpp');
+  const outputPath = path.resolve(__dirname, 'output.json');
 
-test('[WIP] test', async done => {
-  const result = await lizard.analyzeSourceCode('foo.cpp', 'int foo(){}');
-  expect(result).toEqual(expectedResult);
-  done();
+  const input = fs.readFileSync(inputPath, { encoding: 'utf-8' });
+  const output = fs.readFileSync(outputPath, { encoding: 'utf-8' }).replace(/\{filename\}/g, inputPath);
+  const outputObject = JSON.parse(output);
+
+  test('analyzeFile', async done => {
+    expect(await lizard.analyzeFile(inputPath)).toEqual(outputObject);
+    done();
+  });
+
+  test('analyzeSourceCode', async done => {
+    expect(await lizard.analyzeSourceCode(inputPath, input)).toEqual(outputObject);
+    done();
+  });
 });
